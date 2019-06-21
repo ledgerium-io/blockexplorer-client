42a19c8ca337f0922fec7392c05d712e9880a138
Steve <admin@cryptostat.org>
2019-06-21 12:27:44 +1000

    remove unnecessary components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,12 @@
 import React, { Component, Fragment } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
-
-import ColorSwitcher from 'Components/ColorSwitcher'
 import { NotificationContainer } from "Components/ReactNotifications";
-
 import { defaultStartPath } from 'Constants/defaultValues'
-
 import { connect } from "react-redux";
-
 import AppLocale from '../lang';
 import MainRoute from 'Routes';
-import login from 'Routes/pages/login'
-import register from 'Routes/pages/register'
 import error from 'Routes/pages/error'
-import forgotPassword from 'Routes/pages/forgot-password'
-
 import 'Assets/css/vendor/bootstrap.min.css'
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
@@ -56,10 +47,6 @@ class App extends Component {
 								authUser={user}
 								component={MainRoute}
 							/>
-							<Route path={`/login`} component={login} />
-							<Route path={`/register`} component={register} />
-							<Route path={`/forgot-password`} component={forgotPassword} />
-							<Route path={`/error`} component={error} />
 							<Redirect to="/error" />
 						</Switch>
 					</Fragment>
