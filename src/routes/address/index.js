a11d05fb3dad5c82adb3097123e702f7bc93d666
Steve <admin@cryptostat.org>
2019-06-21 12:28:20 +1000

    add in an address route to query an account from backend

---
+++ src/routes/address/index.js
@@ -0,0 +1,171 @@
+import React, { Component, Fragment } from "react";
+import IntlMessages from "Util/IntlMessages";
+import { Row, Card, CardBody,CardHeader, CardTitle, Table, Button, Jumbotron, Badge } from "reactstrap";
+import moment from 'moment'
+moment.updateLocale('en', {
+    relativeTime : {
+        future: "in %s",
+        past:   "%s ago",
+        s  : '%d seconds',
+        ss : '%d seconds',
+        m:  "a minute",
+        mm: "%d minutes",
+        h:  "an hour",
+        hh: "%d hours",
+        d:  "a day",
+        dd: "%d days",
+        M:  "a month",
+        MM: "%d months",
+        y:  "a year",
+        yy: "%d years"
+    }
+});
+import { Colxx, Separator } from "Components/CustomBootstrap";
+import BreadcrumbContainer from "Components/BreadcrumbContainer";
+import io from 'socket.io-client';
+import { NavLink } from "react-router-dom";
+import Web3 from 'web3';
+const web3 = new Web3(new Web3.providers.HttpProvider('http://testnet.ledgerium.net:8545/'));
+import axios from 'axios';
+
+export default class extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      connected: false,
+      loading: false,
+      address: {
+        address: '0x0000000000000000000000000000000000000000',
+        balance: 0,
+        blockNumber: 0,
+        transactions: [],
+        type: 0
+      }
+    }
+  }
+
+  componentWillMount() {
+    axios.get(`http://localhost:2000/api/address/${this.props.match.params.address}`)
+      .then(response => {
+        if (!response.data.success) return;
+        this.setState({
+          address: response.data.data,
+          loading: false
+        })
+    })
+
+  }
+
+
+  render() {
+    const address = this.state.address
+    return (
+      <Fragment>
+        <div className="d-flex justify-content-between align-items-center">
+        <h3>LEDGERIUM BLOCK EXPLORER</h3>
+        <a href="/app/blockexplorer">
+        <Button color="primary" size="sm" className="mb-2">
+          <i className="iconsminds-arrow-out-left"/> Go back
+        </Button>
+        </a>
+        </div>
+        <Separator className="mb-5" />
+
+        <Row>
+          <Colxx md="7" className="mb-4">
+              <Card>
+                <CardHeader className="p-0 position-relative">
+                  <div className="position-absolute handle card-icon">
+                    <i className="simple-icon-shuffle" />
+                  </div>
+                </CardHeader>
+                <CardBody>
+                  <CardTitle className="mb-0">
+                  Address Details
+                  </CardTitle>
+                  <br/>
+                  <p><strong> {address.address}</strong> </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <span> {address.transactions.length} Transactions Sent/Recieved </span>
+                    <span> Last Balance Update Block #<a href={"/app/block/"+address.blockNumber}>{address.blockNumber} </a></span>
+                  </div>
+                  <p></p>
+                  <div className="progress-bar-circle">
+                  </div>
+                </CardBody>
+              </Card>
+            </Colxx>
+            <Colxx md="5"className="mb-4">
+              <Card>
+                <CardHeader className="p-0 position-relative">
+                  <div className="position-absolute handle card-icon">
+                    <i className="simple-icon-shuffle" />
+                  </div>
+                </CardHeader>
+                <CardBody className="d-flex justify-content-between align-items-center">
+                  <CardTitle className="mb-0">
+                    Balance
+                  </CardTitle>
+                  {address.balance}
+                  <div className="progress-bar-circle">
+                  </div>
+                </CardBody>
+              </Card>
+            </Colxx>
+          </Row>
+
+          { !this.state.loading ? this.state.address.transactions.map((tx, i) => {
+                     return <Row>
+                      <Colxx md="12" key={tx.id}>
+                        <Card className="card d-flex mb-3 side-bar-line-tx">
+                          <div className="d-flex flex-grow-1 min-width-zero">
+                          <CardBody className="align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+                          <NavLink
+                            to="#"
+                            id={`toggler${i}`}
+                            className="list-item-heading mb-0 truncate w-40 w-xs-100  mb-1 mt-1"
+                          >
+
+                          { " " }
+                          <span className="align-middle d-inline-block color-theme-1">Transaction</span>
+                        </NavLink>
+                        <p className="mb-1 text-muted text-small w-15 w-xs-100 ">
+                          <span className="color-theme-2"></span>
+                        </p>
+                        <p className="mb-1 text-muted text-small w-15 w-xs-100">
+                          Block #{tx.blockNumber.toLocaleString()}
+                        </p>
+                        <div className="w-15 w-xs-100">
+                          <Badge pill>
+                            {tx.transaction_type}
+                          </Badge>
+                        </div>
+                        </CardBody>
+                        </div>
+                        <div className="card-body pt-1">
+                          <p className="mb-0">
+                          <a href={'/app/tx/'+tx.hash}>{tx.hash}</a>
+                          </p>
+                          <p className="mb-0">
+                          {tx.from} <i className="iconsminds-arrow-out-right"/> {tx.to}
+                          </p>
+                          <br/>
+                        </div>
+                        </Card>
+                      </Colxx>
+                     </Row>
+                  }) : '0 Transactions'
+                }
+
+
+
+
+
+
+
+
+  </Fragment>
+    );
+  }
+}
