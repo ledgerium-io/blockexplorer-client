ffcf90115b8f4c587bb175091cfc4b4f8a438b89
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-06-27 21:38:38 +1000

    add map component

---
+++ src/components/map/index.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react"
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography,
+} from "react-simple-maps"
+import worldData from './world-50m.json'
+const wrapperStyles = {
+  width: "100%",
+  maxWidth: 980,
+  margin: "0 auto",
+}
+
+class BasicMap extends Component {
+  render() {
+    return (
+      <div style={wrapperStyles}>
+        <ComposableMap
+          projectionConfig={{
+            scale: 205,
+            rotation: [-11,0,0],
+          }}
+          width={980}
+          height={551}
+          style={{
+            width: "100%",
+            height: "250px",
+          }}
+          >
+          <ZoomableGroup center={[0,20]} disablePanning>
+            <Geographies geography={worldData}>
+              {(geographies, projection) => geographies.map((geography, i) => geography.id !== "ATA" && (
+                <Geography
+                  key={i}
+                  geography={geography}
+                  projection={projection}
+                  style={{
+                    default: {
+                      fill: "#ECEFF1",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    hover: {
+                      fill: "#607D8B",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                    pressed: {
+                      fill: "#FF5722",
+                      stroke: "#607D8B",
+                      strokeWidth: 0.75,
+                      outline: "none",
+                    },
+                  }}
+                />
+              ))}
+            </Geographies>
+          </ZoomableGroup>
+        </ComposableMap>
+      </div>
+    )
+  }
+}
+
+export default BasicMap
