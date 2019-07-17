44d37ec2aa308973bda0fae5c512ffdbc25acb3d
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-07-17 13:53:31 +1000

    default networks and connected network

--- src/constants/defaultValues.js
+++ src/constants/defaultValues.js
@@ -9,4 +9,31 @@ export const localeOptions=[
 ];
 export const searchPath = "/app/search"
 export const servicePath = "https://api.coloredstrategies.com"
-export const baseURL = "http://138.197.193.201:2002"
+
+export const defaultNetworks = [
+  {
+    id: 1,
+    name: 'Ledgerium Mainnet',
+    url: 'http://testnet.ledgerium.net:2002'
+  },
+  {
+    id: 2,
+    name: 'Ledgerium Testnet',
+    url: 'http://testnet.ledgerium.net:2002'
+  },
+  {
+    id: 3,
+    name: 'Ledgerium Devnet',
+    url: 'http://138.197.193.201:2002'
+  },
+  {
+    id: 4,
+    name: 'Localhost',
+    url: 'http://localhost:2002'
+  }
+]
+
+
+export const networks = [...defaultNetworks]
+export const baseURL = localStorage.getItem('network') ? JSON.parse(localStorage.getItem('network')).url : defaultNetworks[0].url
+export const connectedNetwork = localStorage.getItem('network') ? JSON.parse(localStorage.getItem('network')) : defaultNetworks[0]
