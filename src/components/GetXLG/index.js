96b0e2d320191dc2fc930a9de1958256a401b1de
Steve <admin@cryptostat.org>
2019-08-19 16:11:19 +1000

    get xlg component for top nav

---
+++ src/components/GetXLG/index.js
@@ -0,0 +1,81 @@
+import React, { Component } from "react"
+import { Redirect } from 'react-router-dom';
+import {networks, connectedNetwork, originalNetworkCount} from 'Constants/defaultValues'
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  ModalHeader,
+  ModalBody,
+  Form,
+  Input,
+  Button,
+  ModalFooter,
+  Modal,
+  Label,
+  FormGroup,
+  FormText,
+  NavLink
+} from "reactstrap";
+import { Separator } from "Components/CustomBootstrap";
+
+class GetXLG extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      modal: false,
+      customNetworkName: '',
+      customNetworkURL: '',
+      customNetworkError: '',
+      connected: false
+    }
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+      <UncontrolledDropdown className="ml-2">
+        <DropdownToggle
+          caret
+          color="light"
+          size="sm"
+          className="language-button"
+        >
+        Get XLG
+        </DropdownToggle>
+        <DropdownMenu className="mt-3" right>
+        {connectedNetwork.name === "Ledgerium Mainnet" ?
+          <div>
+            <a target="_blank" href="https://idex.market/eth/xlg">
+              <DropdownItem>
+              ACX <i className="simple-icon-login"/>
+              </DropdownItem>
+            </a>
+            <a target="_blank" href="https://acx.io/markets/xlgbtc">
+              <DropdownItem>
+              IDEX <i className="simple-icon-login"/>
+              </DropdownItem>
+            </a>
+          </div>
+        :
+
+          <div>
+            <NavLink to="/app/faucet">
+              Faucet
+            </NavLink>
+
+          </div>
+        }
+
+        </DropdownMenu>
+      </UncontrolledDropdown>
+      </div>
+    )
+  }
+}
+
+export default GetXLG
