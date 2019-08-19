ec5e463d6949276b30920f4ca07a347ab7ef95d4
Steve <admin@cryptostat.org>
2019-08-19 16:11:09 +1000

    init functions component

---
+++ src/components/Functions/index.js
@@ -0,0 +1,20 @@
+import Web3 from 'web3';
+
+function addressType(address) {
+  return new Promise((resolve) => {
+    if(!address) {
+      resolve('Contract Creation')
+    }
+    web3.eth.getCode(address, (code) => {
+      if(code === "0x") {
+        resolve("Contract Call")
+      } else {
+        resolve("Transaction")
+      }
+    })
+  })
+}
+
+export {
+   addressType
+};
