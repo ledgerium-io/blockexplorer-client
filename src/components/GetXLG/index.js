606707fc270086825053247deb46bb1fd2d5c3f3
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-08-20 11:06:31 +1000

    fix logic for when faucet or buy comes up

--- src/components/GetXLG/index.js
+++ src/components/GetXLG/index.js
@@ -48,7 +48,7 @@ class GetXLG extends Component {
         Get XLG
         </DropdownToggle>
         <DropdownMenu className="mt-3" right>
-        {connectedNetwork.name === "Ledgerium Mainnet" ?
+        {connectedNetwork.name !== "Ledgerium Testnet" && connectedNetwork.name !== "Ledgerium Devnet" ?
           <div>
             <a target="_blank" href="https://idex.market/eth/xlg">
               <DropdownItem>
